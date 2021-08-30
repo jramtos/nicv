3037f9832ae9dfb1a4475b76b194141fc920843e
Jesica Ramirez <jramireztoscano@uchicago.edu>
2021-08-30 17:45:34 -0600

    updating

---
+++ programas/global_data.py
@@ -0,0 +1,65 @@
+# Global NICV
+
+# Author: Jesica Ramirez
+
+import requests
+import pandas as pd
+import io
+import datetime as dt
+
+GLOBAL_BIWEEKLY = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/biweekly_cases_per_million.csv'
+
+
+def get_data():
+    '''
+    Obtain all biweekly cases and calculate NICV for all Countries. 
+
+    Returns: DataFrame
+    '''
+    # Obtain information
+    r = requests.get(GLOBAL_BIWEEKLY)
+    rawCases = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+
+    # Convertir a un formato manipulable
+    raw_long = pd.melt(rawCases, id_vars=[
+                       'date'], value_vars=rawCases.columns[1:])
+    raw_long.rename(columns={'variable': 'location', 'value': 'biweekly_million'},
+                    inplace=True)
+    # Computation of NICV
+    raw_long['nicv'] = raw_long['biweekly_million']*1000/1000000
+
+    # Filtrar columnas relevantes y cambiar formato
+    global_csv = raw_long[['date', 'location', 'nicv']]
+    global_csv = global_csv.rename(columns={'location': 'Country'})
+
+    # Quitar Summer Olympics
+    global_csv = global_csv[(global_csv.Country != '2020 Summer Olympics athletes & staff') &
+                            (global_csv.Country != 'International')]
+
+    # Poner World al principio
+    global_csv = global_csv.loc[global_csv.Country == 'World'].append(
+        global_csv).drop_duplicates().reset_index(drop=True)
+
+    # Filtrar solo fechas deseadas
+    global_csv['date'] = pd.to_datetime(global_csv['date'])
+    end = dt.datetime.today()
+    start = end - dt.timedelta(days=368)
+
+    global_csv_final = global_csv[global_csv['date'] >= start].pivot(
+        index='date', columns='Country', values=['nicv'])
+
+    global_csv_final.columns = global_csv_final.columns.droplevel()
+    global_csv_final.reset_index(inplace=True)
+    global_csv_final.columns.name = ''
+
+    # Guardar archivo como csv
+    global_csv_final.to_csv('1-1-NICV-World.csv', index=False)
+
+    # Print result
+    print('Data has: {} rows and {} columns'.format(
+        global_csv_final.shape[0], global_csv_final.shape[1]))
+    print(global_csv_final)
+
+
+if __name__ == "__main__":
+    get_data()
---
+++ programas/us_data.py
@@ -0,0 +1,84 @@
+# United States NICV
+
+# Author: Jesica Ramirez
+
+import requests
+import pandas as pd
+import numpy as np
+import datetime as dt
+import io
+
+url_county_us = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv"
+
+
+def get_us_data():
+    # Establecer Fechas (Hoy hasta menos un año)
+    end = dt.datetime.today()
+    step = dt.timedelta(days=1)
+    start = end - dt.timedelta(days=368)
+    dates = []
+    while start < end:
+        dates.append(start.strftime('%m-%d-%Y'))
+        start += step
+
+    # Bajar cada hoja de información
+    bigraw = None
+    for i, date in enumerate(dates[:-1]):
+        r = requests.get(url_county_us.format(date))
+        rawData = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+        rawData = rawData[rawData.Country_Region == 'US'][['FIPS', 'Admin2',
+                                                           'Province_State', 'Country_Region', 'Combined_Key',
+                                                           'Lat', 'Long_', 'Confirmed']]
+        rawData['date'] = date
+        if i == 0:
+            bigraw = rawData
+        else:
+            bigraw = bigraw.append(rawData)
+
+    # Bajar información de Población
+    r = requests.get(
+        "https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=county:*")
+    populations = pd.DataFrame(r.json()[1:], columns=r.json()[0])
+    populations['FIPS'] = (populations['state'] +
+                           populations['county']).astype(float).astype(str)
+    populations['POP'] = populations['POP'].astype(float)
+
+    bigraw['FIPS'] = bigraw.groupby(['Combined_Key'])['FIPS'].transform(lambda x:
+                                                                        x.fillna(x.astype(float).min())).astype(str)
+
+    # Juntar todo y calcular 14 dias
+    big = bigraw.merge(populations)
+    big['date'] = pd.to_datetime(big['date'])
+    big = big.sort_values(by=['Combined_Key', 'date']).reset_index(drop=True)
+    big.rename(columns={'Admin2': 'County',
+                        'Province_State': 'State'}, inplace=True)
+
+    # States
+    states = big.groupby(['State', 'date']).agg(
+        {'Confirmed': 'sum', 'POP': 'sum'}).reset_index()
+    vals = states.groupby(['State']).apply(lambda x:
+                                           x.set_index('date')).rolling(14)['Confirmed'].apply(lambda x: x[-1] - x[0],
+                                                                                               raw=True).reset_index()['Confirmed']
+    states['last14'] = vals
+    states['nicv'] = states['last14']/states['POP']*1000
+    states = states.pivot(index='date', columns='State',
+                          values='nicv').reset_index()
+    states.to_csv('2-1-NICV-Estados-USA.csv', index=False)
+
+    # Counties
+    big = big[big.County != 'Unassigned']
+    counties = big.groupby(['Combined_Key', 'date']).agg(
+        {'Confirmed': 'sum', 'POP': 'sum'}).reset_index()
+    vals = counties.groupby(['Combined_Key']).apply(lambda x:
+                                                    x.set_index('date'))\
+        .rolling(14)['Confirmed'].apply(lambda x: x[-1] - x[0],
+                                        raw=True).reset_index()['Confirmed']
+    counties['last14'] = vals
+    counties['nicv'] = counties['last14']/counties['POP']*1000
+    counties = counties.pivot(
+        index='date', columns='Combined_Key', values='nicv').reset_index()
+    counties.to_csv('2-1-NICV-Counties-USA.csv', index=False)
+
+
+if __name__ == "__main__":
+    get_us_data()
